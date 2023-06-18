3dc5cfe087b0c2897fed0803fc880c446249bef8
Alin Dondera <dondera.alin@gmail.com>
2023-06-18 07:52:42 +0200

    Remove image and add metrics related to laplacian

--- solo/losses/manifold_regularizer.py
+++ solo/losses/manifold_regularizer.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import wandb
@@ -9,10 +10,24 @@ def manifold_regularizer_loss(x: torch.Tensor, y: torch.Tensor, log_laplacian: b
     weights_matrix = get_similarity_matrix(x, rbf_scale=1.0, scaling_factor=False)
     laplacian = get_laplacian(weights_matrix, normalized=True)
     if log_laplacian:
-        fig = figure.Figure()
-        ax = fig.subplots(1)
-        ax.imshow(-laplacian.detach().cpu().numpy())
-        wandb.log({"laplacian": fig})
+        with torch.no_grad():
+            sorted_eigvals = torch.linalg.eigvals(laplacian).real.cpu().numpy()
+            sorted_eigvals.sort()
+            zero_eigvals = sorted_eigvals.size - np.count_nonzero(sorted_eigvals)
+            second_smallest_eigenvalue = sorted_eigvals[1]
+            spectral_gap = sorted_eigvals[1] - sorted_eigvals[0]
+            laplacian_energy = sum(abs(x - 1) for x in sorted_eigvals)
+
+            metrics = {
+                "Number of zero eigenvalues": zero_eigvals,
+                "Second smallest eigenvalue": second_smallest_eigenvalue,
+                "Spectral gap": spectral_gap,
+                "Laplacian energy": laplacian_energy,
+            }
+            # fig = figure.Figure()
+            # ax = fig.subplots(1)
+            # ax.imshow(-laplacian.detach().cpu().numpy())
+            wandb.log(metrics)
 
     # D = torch.diag(weights_matrix.sum(dim=-1))
     # subspace = y.T @ D @ y
